98a996425e75faf446b8dc40d099afad0cd434de
Javlonbek <fayzullaevjavlonbek.d@gmail.com>
2023-12-01 19:22:25 +0500

    updated

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -7,15 +7,16 @@ import 'firebase/compat/database';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD14YnNYMPyounPiN_riD-J5sTTePX7Sow",
-  authDomain: "iot2023a-d0419.firebaseapp.com",
-  databaseURL: "https://iot2023a-d0419-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "iot2023a-d0419",
-  storageBucket: "iot2023a-d0419.appspot.com",
-  messagingSenderId: "1082064392036",
-  appId: "1:1082064392036:web:3f8e06b229c8c4c415f471",
-  measurementId: "G-WCLQCCQK17"
+  apiKey: "AIzaSyBefAFEh1NVnt2YaSBn9KMkmKWy6ieZKV8",
+  authDomain: "iot2023-7a185.firebaseapp.com",
+  databaseURL: "https://iot2023-7a185-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "iot2023-7a185",
+  storageBucket: "iot2023-7a185.appspot.com",
+  messagingSenderId: "766170917410",
+  appId: "1:766170917410:web:acb8a3c7663131f16ab87b",
+  measurementId: "G-5H6EMVGPHY"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
